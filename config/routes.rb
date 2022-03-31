b909487531cee6d7a1d3e7128115933599f650da
yuseipen0716 <yuseipen1@gmail.com>
2022-03-31 14:42:22 +0900

    名前空間を用いてadminとgeneralの権限を分けてみる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  resources :articles, only: [:index, :show]
+  namespace :admins do
+    resources :articles 
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
